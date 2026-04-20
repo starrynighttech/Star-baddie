c2af7a220e097c5003e38a5ba7ad42aac51bf185
starrynighttech <starrynight8531@gmail.com>
2026-04-20 18:58:50 +0800

    Update App.jsx

--- apps/web/App.jsx
+++ apps/web/App.jsx
@@ -1,1 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Home from "./pages/Home"
+import Dashboard from "./pages/Dashboard"
+import Ads from "./pages/Ads"
+import Tracking from "./pages/Tracking"
+import Referral from "./pages/Referral"
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/ads" element={<Ads />} />
+        <Route path="/tracking" element={<Tracking />} />
+        <Route path="/referral" element={<Referral />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
